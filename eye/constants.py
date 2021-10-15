b92ece7dcd366cd7f7ef5aa980e496cf2404c39c
Nathaniel Graham <nathaniel.graham@protonmail.com>
2021-10-14 18:55:12 -0600

    constants, not configs

---
+++ eye/constants.py
@@ -0,0 +1,13 @@
+PROGRAM_NAME_FULL="Noita Eyes Cipher"
+PROGRAM_NAME_SHORT="necpy"
+PROGRAM_VERSION="0.1.0"
+AUTHOR_NAME="Nathaniel Graham"
+AUTHOR_EMAIL="nathaniel.graham@protonmail.com"
+AUTHOR_ALIASES=["ngraham20","Raevenant"]
+EYE=["        ██████        ",
+     "    ████      ████    ",
+     "  ██      ██      ██  ",
+     "██      ██████      ██",
+     "  ██      ██      ██  ",
+     "    ████      ████    ",
+     "        ██████        "]
--- eye/ui.py
+++ eye/ui.py
@@ -1,5 +1,24 @@
+import constants
+import json
+
 class UI:
     size=41
+    conf="conf.json"
+
+    @staticmethod
+    def program_header(conf=None, size=None):
+        conf = conf or UI.conf
+        size = size or UI.size
+
+        print('─'*size)
+        print(" " + constants.PROGRAM_NAME_FULL + " : Version " + constants.PROGRAM_VERSION)
+        print(" by " + constants.AUTHOR_NAME +
+        " (@" + (', @'.join(constants.AUTHOR_ALIASES))+")")
+        print('─'*size)
+        for segment in constants.EYE:
+            print(' '*((size-len(segment))//2) + segment)
+        print('─'*size)
+
 
     @staticmethod
     def header(header, size=None):
