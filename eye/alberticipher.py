ddbcb6594395f3ea5ee68d105ca2df8656974db8
Nathaniel Graham <nathaniel.graham@protonmail.com>
2021-10-18 17:24:19 -0600

    alberti cipher works

---
+++ eye/alberticipher.py
@@ -0,0 +1,20 @@
+from wheelcipher import WheelCipher
+from wheel import Wheel
+
+class AlbertiCipher(WheelCipher):
+    def encode(self, plaintext, history=None):
+        """
+        For each letter in plaintext
+            for each wheel
+                propegate the letter through to the last wheel
+        """
+        ciphertext = ""
+        for letter in plaintext:
+            index = self.wheels[0].data.find(letter)
+            if index == -1:
+                ciphertext += letter
+            else:
+                ciphertext += self.wheels[1].data[index]
+                self.advance_wheels(self.wheels, Wheel.Direction.CLOCKWISE)
+            AlbertiCipher.record(history, ciphertext)
+        return ciphertext
